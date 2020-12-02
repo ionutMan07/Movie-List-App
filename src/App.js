adbdbc744c05f33cb37e2c505ab1ded2f6598eba
Ionut V <67919936+ionutMan07@users.noreply.github.com>
2020-12-02 18:21:26 +0200

    done

--- src/App.js
+++ src/App.js
@@ -5,33 +5,40 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import HeaderSearchAppBar from './shared/Header';
 import axios from 'axios';
+import { ContactSupportOutlined } from '@material-ui/icons';
 
 const App = () => {
-
   const [movies, setMovies] = useState([]);
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState('star wars');
 
-  const searchMovies = (searchValue) => {
-    const apiKey = process.env.REACT_APP_API_KEY
-    const URL = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=en-US&query=${searchValue}`    
-    if(axios.get(URL)){
-      return setMovies(axios.get(URL))
-    }
-  }
-    useEffect(() =>{
-      searchMovies(searchValue);
-    },[searchValue]);
+  const searchMovies = async (searchValue) => {
+    const apiKey = process.env.REACT_APP_API_KEY;
+    const URL = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=en-US&query=${searchValue}`;
+    const response = await fetch(URL);
+    const responseJson = await response.json();
+    
+   if(responseJson.results){
+     setMovies(responseJson.results)
+  };
+}
+
+  useEffect(() => {
+    searchMovies(searchValue);
+  }, [searchValue]);
 
   return (
-  <div className= 'container-fluid movie-app'>
-    <div className ='row'>
-      <HeaderSearchAppBar searchValue={searchValue} setSearchValue={setSearchValue}/>
+    <div className="container-fluid movie-app">
+      <div className="col">
+        <HeaderSearchAppBar
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+        />
+      </div>
+      <div className="col">
+        <MovieList movies={movies} />
+      </div>
     </div>
-    <div className = 'row'>
-    <MovieList movies = {movies} />
-    </div>
-  </div>
   );
-}
+};
 
 export default App;
--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -1,22 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 const MovieList = (props) => {
-
-    return (
-        <div>
-        {props.movies.map((movie,index)=> (
-        <div className='d-flex justify-content-start m-3'>
-            <div>
-            <span><h6><b>{movie.title}</b></h6></span>
-            <span>{movie.release_date}</span>
-            </div>
-            <div>
-                <img src={`https://image.tmdb.org/t/p/w154${movie.poster_path}`} alt = {movie.title}></img>
-            </div>
+  return (
+    <div>
+      {props.movies.map((movie, index) => (
+        <div className="d-flex justify-content-start m-3">
+          <img
+            src={`https://image.tmdb.org/t/p/w154${movie.poster_path}`}
+            alt={movie.title}
+          ></img>
+          <div>
+            <span>
+              <b>{movie.title}</b>
+            </span>
+            <br></br>
+            <span> Release date: {movie.release_date}</span>
+          </div>
+          <div></div>
         </div>
-        ))}
-        </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 
-export default MovieList
+export default MovieList;
