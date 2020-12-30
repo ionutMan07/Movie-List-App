73ec1401d167a16957a648c19141e67daf4f2a88
Ionut V <67919936+ionutMan07@users.noreply.github.com>
2020-12-30 13:12:00 +0200

    added dark mode

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
@@ -6,11 +6,35 @@ import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import Switch from '@material-ui/core/Switch';
 
 import { useStyles } from '../Styles/HeaderCss';
 
 const HeaderSearchBar = (props) => {
   const classes = useStyles();
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const json = localStorage.getItem('site-dark-mode');
+    const currentMode = JSON.parse(json);
+    if (currentMode) {
+      setDarkMode(true);
+    } else {
+      setDarkMode(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+    const json = JSON.stringify(darkMode);
+    localStorage.setItem('site-dark-mode', json);
+  }, [darkMode]);
 
   return (
     <div className={classes.root}>
@@ -27,6 +51,17 @@ const HeaderSearchBar = (props) => {
           <Typography className={classes.title} variant="h6" noWrap>
             Movie 📽️ List
           </Typography>
+          <FormGroup>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={darkMode}
+                  onChange={() => setDarkMode(!darkMode)}
+                  aria-label="color switch"
+                />
+              }
+            />
+          </FormGroup>
           <div className={classes.search}>
             <div className={classes.searchIcon}>
               <SearchIcon />
