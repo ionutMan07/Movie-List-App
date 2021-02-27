3ca569a2ffda1eb99a644abee98133d185dfe581
Ionut V <67919936+ionutMan07@users.noreply.github.com>
2021-02-27 14:00:03 +0100

    added key

--- src/Components/Ratings.js
+++ src/Components/Ratings.js
@@ -1,29 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import StarIcon from '@material-ui/icons/StarOutlined';
 
-const useStateWithLocalStorage =  (local) => {
-  const [rating, setRating] = useState(
-    localStorage.getItem(local) || null);
- 
+const useStateWithLocalStorage = (local) => {
+  const [rating, setRating] = useState(localStorage.getItem(local) || null);
+
   useEffect(() => {
     localStorage.setItem(local, rating);
-  }, [rating,local]);
- 
+  }, [rating, local]);
+
   return [rating, setRating];
 };
 
 const Ratings = (props) => {
   const [rating, setRating] = useStateWithLocalStorage(props.movie.id);
   const [hover, setHover] = useState(null);
-  
-  const setRatings = event => setRating(event.target.value);
- 
+
+  const setRatings = (event) => setRating(event.target.value);
+
   return (
     <>
       {[...Array(5)].map((star, index) => {
         const ratingValue = index + 1;
         return (
-          <label>
+          <label key={index}>
             <input
               type="radio"
               name="rating"
